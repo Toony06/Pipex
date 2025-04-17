8964df087fd300b93d3886306b00b864e2e8805e
Tony Roman <toroman@c2r10p1.42nice.fr>
2025-04-17 12:44:43 +0200

    push

---
+++ parsing/Pipex.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: toroman <toroman@student.42nice.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 12:38:27 by toroman           #+#    #+#             */
+/*   Updated: 2025/04/17 12:40:33 by toroman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+# include "utils/header/libft.h"
+# include "utils/header/ft_printf.h"
+# include <fcntl.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+#endif
